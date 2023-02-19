26c1aaed5527444ae5a87f36f54f7437c8d8707e
astroboy20 <tolulopeakinkunmi7@gmail.com>
2023-02-19 18:34:11 +0100

    fetching the banner data

--- features/Banner/Banner.tsx
+++ features/Banner/Banner.tsx
@@ -1,8 +1,28 @@
-import React from 'react'
+'use client'
+
+import axios from 'components/AxioxInstance/axios'
+import requests from 'components/AxioxInstance/request'
+import React,{useEffect, useState} from 'react'
 import { BannerBody, BannerButton, BannerContent, BannerDescription, BannerFadeButton, BannerTitle } from './Banner.style'
 
 
 const Banner = () => {
+  const [movie,setMovie] = useState([])
+
+  useEffect(() => {
+   async function fetchData() {
+    const request = await axios .get(requests.fetchNetflixOriginal)
+
+    setMovie(
+      request.data.results[
+        Math.floor(Math.random() * request.data.result.length -1)
+      ]
+    )
+    return request
+   }
+   fetchData()
+  }, [])
+  console.log(movie)
   //to truncate the description text
   const truncate=(string:string, n:number)=>{
     return string?.length > n ? string.substring(0, n -1) + '...' : string
